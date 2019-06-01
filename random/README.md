5a38eb477ec132497e7e3bb04a9252409d2a8c46
Stan Hughes <hughes.stan@gmail.com>
2019-06-01 08:28:04 -0400

    Created a couple random functions
    Actually 3 random function.  The first gives a random number between 1 to N, second between 1 to 5, and last using the second to get numbers between 1 to 7

---
+++ random/README.md
@@ -0,0 +1,34 @@
+The purpose of this code is to create two functions.
+
+One that return integers from 1 to 5. The second function uses the
+first to return a number between 1 to 7.
+
+An interesting note is that it seems like ones are less likely.
+
+Also seems like sevens are less likely.
+
+Ones only consist of a 1 and a 0; only one why to get a one.
+
+Same for sevens.
+
+Possible ways to get each Number
+
+Ones, 1 + 0
+
+Twos, 1 + 1, 2 + 0
+
+Threes, 1 + 2, 2 + 1, 3 + 0
+
+Fours, 2 + 2, 3 + 1, 4 + 0
+
+Fives, 3 + 2, 4 + 1, 5 + 0
+
+Sixes, 4 + 2, 5 + 1, 6 + 0
+
+Sevens, 6 + 1
+
+1/16 chance for a one or 7 (2/16)
+
+2/16 change for a Two (2/16)
+
+3/16 changes for a three, four, five or six (12/16)
---
+++ random/random.js
@@ -0,0 +1,55 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+function random5() {
+  let randomNum = 0;
+  while (randomNum <= 0) {
+    randomNum = getRandomInt(5+1)
+  }
+  return (
+    randomNum
+  )
+}
+
+function random7() {
+  // Number between 1 to 5
+  let randomNum5 = random5()
+  // Number between 0 to 2
+  let randomNum2
+
+  //
+  // Get a random number between 0 to 2
+  //
+
+  randomNum2 = 4
+  while (randomNum2 > 3 ) {
+    randomNum2 = random5()
+  }
+
+  // We now have some random number between 1 to 3
+  // Change it to a number between 0 to 2
+  randomNum2 = randomNum2 - 1
+
+  // 1 to 5 plus 0 to 2 = 1 to 7
+  let randomResult = randomNum2 + randomNum5
+
+  return randomResult
+}
+
+function displayRandom(number) {
+  let myFunc
+  if (number === 5) {
+    myFunc = random5;
+  } else {
+    myFunc = random7;
+  }
+
+  for (let i=0; i<80; i++) {
+    console.log('' + number + ': ' + myFunc())
+  }
+}
+
+console.log('Random 5 and Random 7')
+displayRandom(5)
+displayRandom(7)
